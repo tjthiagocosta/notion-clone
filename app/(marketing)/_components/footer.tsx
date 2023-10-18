de9e33ebbea9434a81366023ca6f54bc2d728db0
Thiago Costa <86612543+tjthiagocosta@users.noreply.github.com>
2023-10-18 13:33:21 -0300

    add component footer.tsx

---
+++ app/(marketing)/_components/footer.tsx
@@ -0,0 +1,20 @@
+import Logo from "./logo";
+import { Button } from "@/components/ui/button";
+
+const Footer = () => {
+  return (
+    <div className="z-50 flex w-full items-center bg-transparent p-6 dark:bg-[#1f1f1f] ">
+      <Logo />
+      <div className="flex w-full items-center justify-between gap-x-2 text-muted-foreground md:ml-auto md:justify-end ">
+        <Button variant="ghost" size="sm">
+          Privacy Policy
+        </Button>
+        <Button variant="ghost" size="sm">
+          Terms & Conditions
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
