e815a2aca5acc5e4ee9979cb38d731a7a0617067
Thiago Costa <86612543+tjthiagocosta@users.noreply.github.com>
2023-10-18 13:37:48 -0300

    add Heading and Footer to MarketingPage

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,17 @@
+import Heading from "./_components/heading";
+import Heroes from "./_components/heroes";
+import Footer from "./_components/footer";
+
+const MarketingPage = () => {
+  return (
+    <div className="flex min-h-full flex-col">
+      <div className="flex flex-1 flex-col items-center justify-center gap-y-8 px-6 pb-10 text-center md:justify-start">
+        <Heading />
+        <Heroes />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default MarketingPage;
