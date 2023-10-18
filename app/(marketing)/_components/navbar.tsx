6f84055c10f7110e52701dc657945690a2996b81
Thiago Costa <86612543+tjthiagocosta@users.noreply.github.com>
2023-10-18 16:24:40 -0300

    add component navbar.tsx

---
+++ app/(marketing)/_components/navbar.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { cn } from "@/lib/utils";
+import Logo from "./logo";
+import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
+
+const NavBar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  const scrolled = useScrollTop();
+
+  return (
+    <div
+      className={cn(
+        "fixed top-0 z-50 flex w-full items-center bg-background p-6 dark:bg-[#1f1f1f]",
+        scrolled && "border-b shadow-sm",
+      )}
+    >
+      <Logo />
+      <div className="flex w-full items-center justify-between gap-x-2  md:ml-auto md:justify-end">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Potion free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Potion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
+        <ModeToggle />
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
